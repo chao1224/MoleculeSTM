9ba1584489369cd18bdc18d197e3bfdac6145d68
chao1224 <shengchao.hacker@gmail.com>
2024-04-19 01:25:21 -0400

    Code clean-ups, #24

--- MoleculeSTM/datasets/utils.py
+++ MoleculeSTM/datasets/utils.py
@@ -4,60 +4,26 @@ import numpy as np
 import torch
 from rdkit import Chem
 from torch_geometric.data import Data
-
-
-allowable_features = {
-    'possible_atomic_num_list':       list(range(1, 119)),
-    'possible_formal_charge_list':    [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5],
-    'possible_chirality_list':        [
-        Chem.rdchem.ChiralType.CHI_UNSPECIFIED,
-        Chem.rdchem.ChiralType.CHI_TETRAHEDRAL_CW,
-        Chem.rdchem.ChiralType.CHI_TETRAHEDRAL_CCW,
-        Chem.rdchem.ChiralType.CHI_OTHER
-    ],
-    'possible_hybridization_list':    [
-        Chem.rdchem.HybridizationType.S,
-        Chem.rdchem.HybridizationType.SP,
-        Chem.rdchem.HybridizationType.SP2,
-        Chem.rdchem.HybridizationType.SP3,
-        Chem.rdchem.HybridizationType.SP3D,
-        Chem.rdchem.HybridizationType.SP3D2,
-        Chem.rdchem.HybridizationType.UNSPECIFIED
-    ],
-    'possible_numH_list':             [0, 1, 2, 3, 4, 5, 6, 7, 8],
-    'possible_implicit_valence_list': [0, 1, 2, 3, 4, 5, 6],
-    'possible_degree_list':           [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    'possible_bonds':                 [
-        Chem.rdchem.BondType.SINGLE,
-        Chem.rdchem.BondType.DOUBLE,
-        Chem.rdchem.BondType.TRIPLE,
-        Chem.rdchem.BondType.AROMATIC
-    ],
-    'possible_bond_dirs':             [  # only for double bond stereo information
-        Chem.rdchem.BondDir.NONE,
-        Chem.rdchem.BondDir.ENDUPRIGHT,
-        Chem.rdchem.BondDir.ENDDOWNRIGHT
-    ]
-}
+from ogb.utils.features import atom_to_feature_vector, bond_to_feature_vector
 
 
 def mol_to_graph_data_obj_simple(mol):
+    """ used in MoleculeNetGraphDataset() class
+    Converts rdkit mol objects to graph data object in pytorch geometric
+    NB: Uses simplified atom and bond features, and represent as indices
+    :param mol: rdkit mol object
+    :return: graph data object with the attributes: x, edge_index, edge_attr """
+
     # atoms
-    # num_atom_features = 2  # atom type, chirality tag
     atom_features_list = []
     for atom in mol.GetAtoms():
-        atomic_num = atom.GetAtomicNum()
-        chiral_tag = atom.GetChiralTag()
-        if atomic_num == 0:
-            atomic_num = 118 # Only for one extreme case
-        atom_feature = [allowable_features['possible_atomic_num_list'].index(atomic_num)] + \
-                       [allowable_features['possible_chirality_list'].index(chiral_tag)]
+        atom_feature = atom_to_feature_vector(atom)
         atom_features_list.append(atom_feature)
     x = torch.tensor(np.array(atom_features_list), dtype=torch.long)
 
     # bonds
     if len(mol.GetBonds()) <= 0:  # mol has no bonds
-        num_bond_features = 2  # bond type & direction
+        num_bond_features = 3  # bond type & direction
         edge_index = torch.empty((2, 0), dtype=torch.long)
         edge_attr = torch.empty((0, num_bond_features), dtype=torch.long)
     else:  # mol has bonds
@@ -66,12 +32,8 @@ def mol_to_graph_data_obj_simple(mol):
         for bond in mol.GetBonds():
             i = bond.GetBeginAtomIdx()
             j = bond.GetEndAtomIdx()
-            bond_type = bond.GetBondType()
-            bond_dir = bond.GetBondDir()
-            if bond_dir not in allowable_features['possible_bond_dirs']:
-                bond_dir = 0
-            edge_feature = [allowable_features['possible_bonds'].index(bond_type)] + \
-                           [allowable_features['possible_bond_dirs'].index(bond_dir)]
+            edge_feature = bond_to_feature_vector(bond)
+
             edges_list.append((i, j))
             edge_features_list.append(edge_feature)
             edges_list.append((j, i))
